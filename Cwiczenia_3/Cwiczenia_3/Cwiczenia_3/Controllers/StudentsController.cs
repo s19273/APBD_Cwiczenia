9baab9f7bdf4b84009434a26b20db93eecfa74b8
s19273 <s19273@pjwstk.edu.pl>
2020-04-02 11:37:28 +0200

    Cwiczenia 4
    Wykonałem ćwiczenia 4

--- Cwiczenia_3/Cwiczenia_3/Cwiczenia_3/Controllers/StudentsController.cs
+++ Cwiczenia_3/Cwiczenia_3/Cwiczenia_3/Controllers/StudentsController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cwiczenia_3.DAL;
+using Cwiczenia_3.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Cwiczenia_3.Controllers
 {
@@ -11,6 +15,42 @@ namespace Cwiczenia_3.Controllers
 
     public class StudentsController : ControllerBase
     {
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=s19273;Integrated Security=True";
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var list = new List<Student>();
+            using (var con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+
+                com.CommandText = "select FirstName, LastName, BirthDate, Studies.Name, Semester from Student, Studies, Enrollment where Enrollment.IdEnrollment=Student.IdEnrollment and Studies.IdStudy=Enrollment.IdStudy;";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.Study = dr["Name"].ToString();
+                    st.Semester = dr["Semester"].ToString();
+                    list.Add(st);
+                }
+
+            }
+            return Ok(list);
+        }
+
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
         [HttpGet]
         public string GetStudent(string orderBy)
         {
@@ -19,12 +59,12 @@ namespace Cwiczenia_3.Controllers
 
         [HttpGet("{id}")]
 
-        public IActionResult GetStudent(int id) 
+        public IActionResult GetStudent(int id)
         {
             if (id == 1)
             {
                 return Ok("Kowalski");
-            } 
+            }
             else if (id == 2)
             {
                 return Ok("Malewski");
@@ -35,7 +75,8 @@ namespace Cwiczenia_3.Controllers
 
         [HttpPost]
 
-        public IActionResult CreateStudent(Models.Student student) {
+        public IActionResult CreateStudent(Models.Student student)
+        {
             student.IndexNumber = $"s{new Random().Next(1, 20000)}";
             return Ok(student);
         }
